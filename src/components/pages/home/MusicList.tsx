2f9dc856d8dedd62475efb0aee1186c3e8aa9e1a
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-25 22:25:56 +0700

    Update

--- src/components/pages/home/MusicList.tsx
+++ src/components/pages/home/MusicList.tsx
@@ -1,25 +1,26 @@
 import React from "react"
-import { MDBContainer, MDBAlert } from "mdbreact"
 
-const AlertPage = () => {
+import MusicCard from "./MusicCard"
+
+const MusicList = data => {
   return (
-    <MDBContainer>
-      <MDBAlert color="primary">
-        A simple primary alert with{" "}
-        <a href="#!" className="alert-link">
-          an example link
-        </a>
-        . Give it a click if you like.
-      </MDBAlert>
-      <MDBAlert color="secondary">
-        A simple primary alert with{" "}
-        <a href="#!" className="alert-link">
-          an example link
-        </a>
-        . Give it a click if you like.
-      </MDBAlert>
-    </MDBContainer>
+    <>
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+      <MusicCard data={data} />
+    </>
   )
 }
 
-export default AlertPage
+export default MusicList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,24 +27,17 @@ const IndexPage = () => {
       }
     }
   `)
-  console.log("====================================")
-  console.log(data)
-  console.log("====================================")
 
-  const Items = data.allFlamelinkConsultationContent.nodes
-
-  console.log("====================================")
-  console.log(Items)
-  console.log("====================================")
+  const Items = data.allFlamelinkMusicContent.nodes
 
   return (
     <Layout>
       <SEO title="Home" />
       <ul>
-        {Items.map(value => (
+        {Items.map(item => (
           <li>
             <ReactHowler
-              src="http://goldfirestudios.com/proj/howlerjs/sound.ogg"
+              src="https://storage.googleapis.com/sinhala-music.appspot.com/flamelink/media/Dua%20Lipa.mp3?GoogleAccessId=firebase-adminsdk-5njpr%40sinhala-music.iam.gserviceaccount.com&Expires=16725200400&Signature=fD41SfxnUSjwpM%2FmwR0eRnr1G6s1jC1kQNxm1jfz464qcsVEPjH633zD4wQDQvejQyl%2B5I8ysNDTWfejQDp21%2BUJ95tsm5LfRXwiUwawEDxzahe8czDv5t4t6vBknledRyrD7OymeXN7MZOQwL%2FHDBojvE72Vxfnv%2F3HLRKA78oihUBiqSgMTMM9Nzdwx4tdoGtluJL%2BPmQSer%2FX7WRklSK0qIdSNowfB8BQ%2B7pf2ZY3FvOy6UOO1UdO3xLx0G7iWxgYUXw%2BXm%2BxSf9baP%2FnELJXeZswp0bxVL4W%2B%2FEoVHbjnNM5RXrJxXF5lJHGT2GduFV0AWvHPeXFDW0qGGe3iQ%3D%3D"
               playing={isPlaying}
               loop={true}
             />
@@ -52,7 +45,7 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <MusicList />
+      <MusicList data={Items} />
     </Layout>
   )
 }
