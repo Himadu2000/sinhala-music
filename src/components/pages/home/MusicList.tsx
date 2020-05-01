f9dc205f6e451e46908a7642d7ba011ba1ae8183
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-02 02:27:13 +0700

    Revert "Update MusicList.tsx"
    This reverts commit 2c78f8146546b7f0f0200025f52dc97a508e5ca1.

--- src/components/pages/home/MusicList.tsx
+++ src/components/pages/home/MusicList.tsx
@@ -1,55 +1,21 @@
-import React, { useState } from "react"
-import { MDBBtn, MDBIcon } from "mdbreact"
-
-import ReactHowler from "react-howler"
+import React from "react"
 
 import MusicCard from "./MusicCard"
-import PlayList from "./PlayList"
 
 import "./styles.css"
 
 const MusicList = data => {
   const Items = data.data
-  const Amaradewa = {}
-  if (Items.map(unit => (unit.singer = "W D Amaradewa"))) {
-    //Amaradewa =
-  }
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [icon, setIcon] = useState("play")
-  function play() {
-    setIsPlaying(!isPlaying)
-  }
   return (
     <>
-      <PlayList />
-      <MDBBtn onClick={play}>
-        Play All
-        <MDBIcon icon={icon} />
-      </MDBBtn>
       <p className="name">W D Amaradewa</p>
       <ul className="grid">
         {Items.map(item => (
-          <li
-            key={
-              //Items.map(unit => unit.title)
-              Math.random() * 1000 * Math.random() +
-              Math.random() * Math.pow(24, 42) * 50
-            }
-          >
+          <li key={Items.flamelink_id}>
             <MusicCard data={item} />
           </li>
         ))}
       </ul>
-      <ReactHowler
-        src={Items.map(unit => unit.audioFile.map(itemMap => itemMap.url))}
-        playing={isPlaying}
-        preload={false}
-        html5={true}
-        onPlay={() => setIcon("stop")}
-        onPause={() => setIcon("play")}
-        onStop={() => setIcon("play")}
-        onLoadError={error => console.error(error)}
-      />
     </>
   )
 }
